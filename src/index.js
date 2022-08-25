57bbc860eca71cf671486505eb8d6804be914b67
Nikita Balashov <nikita.89534588306@gmail.com>
2022-08-25 12:28:03 +0500

    create eventListener for button and link state(valueCounter) with button

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 let state = 0;//counter value
 
 const counter=document.getElementById('counter')
+const addBtn = document.getElementById('add')
+const subBtn = document.getElementById('sub') 
+
 const render = () => {
     counter.textContent=state;
 }
-
 render();
+
+addBtn.addEventListener('click', ()=>{
+    state++;
+    render()
+})
+
+subBtn.addEventListener('click', ()=>{
+    state--;
+    render();
+})
